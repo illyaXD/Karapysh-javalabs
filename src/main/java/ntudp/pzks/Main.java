edcb361eea6981537664083e56b55d6c2373d47c
illia <i.karapysh2017@gmail.com>
2025-03-29 23:23:04 +0200

    Added lab1

---
+++ src/main/java/ntudp/pzks/Main.java
@@ -0,0 +1,9 @@
+package ntudp.pzks;
+
+import ntudp.pzks.lr1.HelloWorld;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloWorld.printHelloWorld();
+    }
+}
---
+++ src/main/java/ntudp/pzks/lr1/HelloWorld.java
@@ -0,0 +1,7 @@
+package ntudp.pzks.lr1;
+
+public class HelloWorld {
+    public static void printHelloWorld() {
+        System.out.println("Hello World!");
+    }
+}
